332723dd0bcf9f4b5b682be425f8a5743318a622
IgorVencel <igor.vencel123@gmail.com>
2024-01-30 03:25:03 +0300

    back

--- app/Http/Controllers/Student/AnswerController.php
+++ app/Http/Controllers/Student/AnswerController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        return response(200);
+    }
 }
