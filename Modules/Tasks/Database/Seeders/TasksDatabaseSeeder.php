7f476e59be33a655058eab014daf20e9b2e31a6c
avangero <alex.avangero@gmail.com>
2024-02-09 11:08:23 +0300

    Add dynamicly load seeders

--- Modules/Tasks/Database/Seeders/TasksDatabaseSeeder.php
+++ Modules/Tasks/Database/Seeders/TasksDatabaseSeeder.php
@@ -7,16 +7,14 @@ use Illuminate\Support\Facades\App;
 
 class TasksDatabaseSeeder extends Seeder
 {
-    protected array $prod = [
-        prod\CourseStatuseSeeder::class,
-        prod\TaskStatusSeeder::class,
-    ];
+    protected array $prod = [];
+    protected array $dev = [];
 
-    protected array $dev = [
-        dev\CourseSeeder::class,
-        dev\TasksSeeder::class,
-        dev\CommentsSeeder::class,
-    ];
+    public function __construct()
+    {
+        $this->prod = $this->getSeeders('prod');
+        $this->dev = $this->getSeeders('dev');
+    }
 
     public function run(): void
     {
@@ -27,5 +25,19 @@ class TasksDatabaseSeeder extends Seeder
         }
 
         $this->call($call);
+    }
+
+    protected function getSeeders(string $folder): array
+    {
+        $seeders = [];
+
+        $files = glob(__DIR__ . "/{$folder}/*.php");
+
+        foreach ($files as $file) {
+            $className = __NAMESPACE__ . '\\' . $folder . '\\' . basename($file, '.php');
+            $seeders[] = $className;
+        }
+
+        return $seeders;
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,13 +7,15 @@ use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
-    protected array $prod = [
-        prod\RolesTableSeeder::class,
-    ];
+    protected array $prod = [];
 
-    protected array $dev = [
-        dev\UsersSeeder::class,
-    ];
+    protected array $dev = [];
+
+    public function __construct()
+    {
+        $this->prod = $this->getSeeders('prod');
+        $this->dev = $this->getSeeders('dev');
+    }
 
     public function run(): void
     {
@@ -24,5 +26,19 @@ class DatabaseSeeder extends Seeder
         }
 
         $this->call($call);
+    }
+
+    protected function getSeeders(string $folder): array
+    {
+        $seeders = [];
+
+        $files = glob(__DIR__ . "/{$folder}/*.php");
+
+        foreach ($files as $file) {
+            $className = __NAMESPACE__ . '\\' . $folder . '\\' . basename($file, '.php');
+            $seeders[] = $className;
+        }
+
+        return $seeders;
     }
 }
